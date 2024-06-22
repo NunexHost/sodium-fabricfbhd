f3f2988e3581a5c31edf40979a7722ba7f98af12
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-22 19:02:24 -0300

    Update AoFaceData.java

--- src/main/java/me/jellysquid/mods/sodium/client/model/light/smooth/AoFaceData.java
+++ src/main/java/me/jellysquid/mods/sodium/client/model/light/smooth/AoFaceData.java
@@ -8,40 +8,34 @@ import net.minecraft.util.math.Direction;
 import static me.jellysquid.mods.sodium.client.model.light.data.ArrayLightDataCache.*;
 
 class AoFaceData {
-    public final int[] lm = new int[4];
-
-    public final float[] ao = new float[4];
-    public final float[] bl = new float[4];
-    public final float[] sl = new float[4];
-
+    private final int[] lm = new int[4];
+    private final float[] ao = new float[4];
+    private final float[] bl = new float[4];
+    private final float[] sl = new float[4];
     private int flags;
 
     public void initLightData(LightDataAccess cache, BlockPos pos, Direction direction, boolean offset) {
-        final int x = pos.getX();
-        final int y = pos.getY();
-        final int z = pos.getZ();
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
 
-        final int adjX;
-        final int adjY;
-        final int adjZ;
+        int adjX = x;
+        int adjY = y;
+        int adjZ = z;
 
         if (offset) {
-            adjX = x + direction.getOffsetX();
-            adjY = y + direction.getOffsetY();
-            adjZ = z + direction.getOffsetZ();
-        } else {
-            adjX = x;
-            adjY = y;
-            adjZ = z;
+            adjX += direction.getOffsetX();
+            adjY += direction.getOffsetY();
+            adjZ += direction.getOffsetZ();
         }
 
-        final int adjWord = cache.get(adjX, adjY, adjZ);
+        int adjWord = cache.get(adjX, adjY, adjZ);
 
-        final int calm;
-        final boolean caem;
+        int calm;
+        boolean caem;
 
         if (offset && unpackFO(adjWord)) {
-            final int originWord = cache.get(x, y, z);
+            int originWord = cache.get(x, y, z);
             calm = getLightmap(originWord);
             caem = unpackEM(originWord);
         } else {
@@ -49,38 +43,37 @@ class AoFaceData {
             caem = unpackEM(adjWord);
         }
 
-        final float caao = unpackAO(adjWord);
+        float caao = unpackAO(adjWord);
 
         Direction[] faces = AoNeighborInfo.get(direction).faces;
 
-        final int e0 = cache.get(adjX, adjY, adjZ, faces[0]);
-        final int e0lm = getLightmap(e0);
-        final float e0ao = unpackAO(e0);
-        final boolean e0op = unpackOP(e0);
-        final boolean e0em = unpackEM(e0);
+        int e0 = cache.get(adjX, adjY, adjZ, faces[0]);
+        int e0lm = getLightmap(e0);
+        float e0ao = unpackAO(e0);
+        boolean e0op = unpackOP(e0);
+        boolean e0em = unpackEM(e0);
 
-        final int e1 = cache.get(adjX, adjY, adjZ, faces[1]);
-        final int e1lm = getLightmap(e1);
-        final float e1ao = unpackAO(e1);
-        final boolean e1op = unpackOP(e1);
-        final boolean e1em = unpackEM(e1);
+        int e1 = cache.get(adjX, adjY, adjZ, faces[1]);
+        int e1lm = getLightmap(e1);
+        float e1ao = unpackAO(e1);
+        boolean e1op = unpackOP(e1);
+        boolean e1em = unpackEM(e1);
 
-        final int e2 = cache.get(adjX, adjY, adjZ, faces[2]);
-        final int e2lm = getLightmap(e2);
-        final float e2ao = unpackAO(e2);
-        final boolean e2op = unpackOP(e2);
-        final boolean e2em = unpackEM(e2);
+        int e2 = cache.get(adjX, adjY, adjZ, faces[2]);
+        int e2lm = getLightmap(e2);
+        float e2ao = unpackAO(e2);
+        boolean e2op = unpackOP(e2);
+        boolean e2em = unpackEM(e2);
 
-        final int e3 = cache.get(adjX, adjY, adjZ, faces[3]);
-        final int e3lm = getLightmap(e3);
-        final float e3ao = unpackAO(e3);
-        final boolean e3op = unpackOP(e3);
-        final boolean e3em = unpackEM(e3);
+        int e3 = cache.get(adjX, adjY, adjZ, faces[3]);
+        int e3lm = getLightmap(e3);
+        float e3ao = unpackAO(e3);
+        boolean e3op = unpackOP(e3);
+        boolean e3em = unpackEM(e3);
 
-        // If neither edge of a corner is occluded, then use the light
-        final int c0lm;
-        final float c0ao;
-        final boolean c0em;
+        int c0lm, c1lm, c2lm, c3lm;
+        float c0ao, c1ao, c2ao, c3ao;
+        boolean c0em, c1em, c2em, c3em;
 
         if (e2op && e0op) {
             c0lm = e0lm;
@@ -93,10 +86,6 @@ class AoFaceData {
             c0em = unpackEM(d0);
         }
 
-        final int c1lm;
-        final float c1ao;
-        final boolean c1em;
-
         if (e3op && e0op) {
             c1lm = e0lm;
             c1ao = e0ao;
@@ -108,12 +97,7 @@ class AoFaceData {
             c1em = unpackEM(d1);
         }
 
-        final int c2lm;
-        final float c2ao;
-        final boolean c2em;
-
         if (e2op && e1op) {
-            // FIX: Use e1 instead of e0 to fix lighting errors in some directions
             c2lm = e1lm;
             c2ao = e1ao;
             c2em = e1em;
@@ -124,12 +108,7 @@ class AoFaceData {
             c2em = unpackEM(d2);
         }
 
-        final int c3lm;
-        final float c3ao;
-        final boolean c3em;
-
         if (e3op && e1op) {
-            // FIX: Use e1 instead of e0 to fix lighting errors in some directions
             c3lm = e1lm;
             c3ao = e1ao;
             c3em = e1em;
@@ -140,27 +119,20 @@ class AoFaceData {
             c3em = unpackEM(d3);
         }
 
-        float[] ao = this.ao;
         ao[0] = (e3ao + e0ao + c1ao + caao) * 0.25f;
         ao[1] = (e2ao + e0ao + c0ao + caao) * 0.25f;
         ao[2] = (e2ao + e1ao + c2ao + caao) * 0.25f;
         ao[3] = (e3ao + e1ao + c3ao + caao) * 0.25f;
 
-        int[] cb = this.lm;
-        cb[0] = calculateCornerBrightness(e3lm, e0lm, c1lm, calm, e3em, e0em, c1em, caem);
-        cb[1] = calculateCornerBrightness(e2lm, e0lm, c0lm, calm, e2em, e0em, c0em, caem);
-        cb[2] = calculateCornerBrightness(e2lm, e1lm, c2lm, calm, e2em, e1em, c2em, caem);
-        cb[3] = calculateCornerBrightness(e3lm, e1lm, c3lm, calm, e3em, e1em, c3em, caem);
+        lm[0] = calculateCornerBrightness(e3lm, e0lm, c1lm, calm, e3em, e0em, c1em, caem);
+        lm[1] = calculateCornerBrightness(e2lm, e0lm, c0lm, calm, e2em, e0em, c0em, caem);
+        lm[2] = calculateCornerBrightness(e2lm, e1lm, c2lm, calm, e2em, e1em, c2em, caem);
+        lm[3] = calculateCornerBrightness(e3lm, e1lm, c3lm, calm, e3em, e1em, c3em, caem);
 
-        this.flags |= AoCompletionFlags.HAS_LIGHT_DATA;
+        flags |= AoCompletionFlags.HAS_LIGHT_DATA;
     }
 
     public void unpackLightData() {
-        int[] lm = this.lm;
-
-        float[] bl = this.bl;
-        float[] sl = this.sl;
-
         bl[0] = unpackBlockLight(lm[0]);
         bl[1] = unpackBlockLight(lm[1]);
         bl[2] = unpackBlockLight(lm[2]);
@@ -171,28 +143,23 @@ class AoFaceData {
         sl[2] = unpackSkyLight(lm[2]);
         sl[3] = unpackSkyLight(lm[3]);
 
-        this.flags |= AoCompletionFlags.HAS_UNPACKED_LIGHT_DATA;
+        flags |= AoCompletionFlags.HAS_UNPACKED_LIGHT_DATA;
     }
 
     public float getBlendedSkyLight(float[] w) {
-        return weightedSum(this.sl, w);
+        return weightedSum(sl, w);
     }
 
     public float getBlendedBlockLight(float[] w) {
-        return weightedSum(this.bl, w);
+        return weightedSum(bl, w);
     }
 
     public float getBlendedShade(float[] w) {
-        return weightedSum(this.ao, w);
+        return weightedSum(ao, w);
     }
 
     private static float weightedSum(float[] v, float[] w) {
-        float t0 = v[0] * w[0];
-        float t1 = v[1] * w[1];
-        float t2 = v[2] * w[2];
-        float t3 = v[3] * w[3];
-
-        return t0 + t1 + t2 + t3;
+        return v[0] * w[0] + v[1] * w[1] + v[2] * w[2] + v[3] * w[3];
     }
 
     private static float unpackSkyLight(int i) {
@@ -204,56 +171,37 @@ class AoFaceData {
     }
 
     private static int calculateCornerBrightness(int a, int b, int c, int d, boolean aem, boolean bem, boolean cem, boolean dem) {
-        // FIX: Normalize corner vectors correctly to the minimum non-zero value between each one to prevent
-        // strange issues
-        if ((a == 0) || (b == 0) || (c == 0) || (d == 0)) {
-            // Find the minimum value between all corners
-            final int min = minNonZero(minNonZero(a, b), minNonZero(c, d));
-
-            // Normalize the corner values
+        if (a == 0 || b == 0 || c == 0 || d == 0) {
+            int min = minNonZero(minNonZero(a, b), minNonZero(c, d));
             a = Math.max(a, min);
             b = Math.max(b, min);
             c = Math.max(c, min);
             d = Math.max(d, min);
         }
 
-        // FIX: Apply the fullbright lightmap from emissive blocks at the very end so it cannot influence
-        // the minimum lightmap and produce incorrect results (for example, sculk sensors in a dark room)
-        if (aem) {
-            a = LightmapTextureManager.MAX_LIGHT_COORDINATE;
-        }
-        if (bem) {
-            b = LightmapTextureManager.MAX_LIGHT_COORDINATE;
-        }
-        if (cem) {
-            c = LightmapTextureManager.MAX_LIGHT_COORDINATE;
-        }
-        if (dem) {
-            d = LightmapTextureManager.MAX_LIGHT_COORDINATE;
-        }
+        if (aem) a = LightmapTextureManager.MAX_LIGHT_COORDINATE;
+        if (bem) b = LightmapTextureManager.MAX_LIGHT_COORDINATE;
+        if (cem) c = LightmapTextureManager.MAX_LIGHT_COORDINATE;
+        if (dem) d = LightmapTextureManager.MAX_LIGHT_COORDINATE;
 
         return ((a + b + c + d) >> 2) & 0xFF00FF;
     }
 
     private static int minNonZero(int a, int b) {
-        if (a == 0) {
-            return b;
-        } else if (b == 0) {
-            return a;
-        }
-
+        if (a == 0) return b;
+        if (b == 0) return a;
         return Math.min(a, b);
     }
 
     public boolean hasLightData() {
-        return (this.flags & AoCompletionFlags.HAS_LIGHT_DATA) != 0;
+        return (flags & AoCompletionFlags.HAS_LIGHT_DATA) != 0;
     }
 
     public boolean hasUnpackedLightData() {
-        return (this.flags & AoCompletionFlags.HAS_UNPACKED_LIGHT_DATA) != 0;
+        return (flags & AoCompletionFlags.HAS_UNPACKED_LIGHT_DATA) != 0;
     }
 
     public void reset() {
-        this.flags = 0;
+        flags = 0;
     }
 }
