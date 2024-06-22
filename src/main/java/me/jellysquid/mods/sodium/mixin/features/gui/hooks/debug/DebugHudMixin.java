484cf2c42bda497bbd3c36c47e976e7004df3359
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-22 18:32:37 -0300

    Update DebugHudMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/hooks/debug/DebugHudMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/hooks/debug/DebugHudMixin.java
@@ -17,11 +17,12 @@ import java.util.ArrayList;
 
 @Mixin(DebugHud.class)
 public abstract class DebugHudMixin {
+
     @Redirect(method = "getRightText", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList([Ljava/lang/Object;)Ljava/util/ArrayList;", remap = false))
     private ArrayList<String> redirectRightTextEarly(Object[] elements) {
         ArrayList<String> strings = Lists.newArrayList((String[]) elements);
         strings.add("");
-        strings.add("%sSodium Renderer (%s)".formatted(getVersionColor(), SodiumClientMod.getVersion()));
+        strings.add(String.format("%sSodium Renderer (%s)", getVersionColor(), SodiumClientMod.getVersion()));
 
         var renderer = SodiumWorldRenderer.instanceNullable();
 
@@ -34,7 +35,6 @@ public abstract class DebugHudMixin {
 
             if (str.startsWith("Allocated:")) {
                 strings.add(i + 1, getNativeMemoryString());
-
                 break;
             }
         }
@@ -43,24 +43,22 @@ public abstract class DebugHudMixin {
     }
 
     @Unique
-    private static Formatting getVersionColor() {
+    private static final Formatting versionColor = calculateVersionColor();
+
+    private static Formatting calculateVersionColor() {
         String version = SodiumClientMod.getVersion();
-        Formatting color;
-
         if (version.contains("-local")) {
-            color = Formatting.RED;
+            return Formatting.RED;
         } else if (version.contains("-snapshot")) {
-            color = Formatting.LIGHT_PURPLE;
+            return Formatting.LIGHT_PURPLE;
         } else {
-            color = Formatting.GREEN;
+            return Formatting.GREEN;
         }
-
-        return color;
     }
 
     @Unique
     private static String getNativeMemoryString() {
-        return "Off-Heap: +" + MathUtil.toMib(getNativeMemoryUsage()) + "MB";
+        return String.format("Off-Heap: +%dMB", MathUtil.toMib(getNativeMemoryUsage()));
     }
 
     @Unique
